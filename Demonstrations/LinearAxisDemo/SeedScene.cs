46426090b1ff3843812e065614530f8ca8e0555e
Olaf Otterbach <olaf.otterbach@gmx.de>
2022-03-20 13:22:16 +0100

    Addin linear axis demonstration.

---
+++ Demonstrations/LinearAxisDemo/SeedScene.cs
@@ -0,0 +1,53 @@
+﻿using Kinematicula.Scening;
+using Kinematicula.Graphics;
+using Kinematicula.Graphics.Creators;
+using Kinematicula.Graphics.Saving;
+using Kinematicula.Kinematics.DirectForwardSolving;
+using Kinematicula.Kinematics.DirectInverseSolving;
+using Kinematicula.Mathematics;
+
+namespace CylinderDemonstration
+{
+    public static class SeedScene
+    {
+        public static Scene CreateAndPopulateScene()
+        {
+            var scene = new Scene(new DirectForwardConstraintSolver(), new DirectInverseConstraintSolver(), new SnapshotFactory(new StateFactory()));
+
+            // floor
+            var floor = Floor.Create(10, 100);
+            floor.Name = "Floor";
+            scene.AddBody(floor);
+            var fixedToWorldConstraint = new FixedConstraint(new Anchor(scene.World, Matrix44D.Identity), new Anchor(floor, Matrix44D.Identity));
+
+
+            // first socket
+            var firstSocket = Cube.Create(100);
+            firstSocket.Name = "first socket";
+            scene.AddBody(firstSocket);
+
+            var fixedFirstSocketToFloor = new FixedConstraint(new Anchor(floor, Matrix44D.CreateTranslation(new Vector3D(-450, 0,0))), new Anchor(firstSocket, Matrix44D.CreateTranslation(new Vector3D(0,0,-50))));
+
+
+
+            // first shifter
+            var firstShifter = Cuboid.Create(500, 100, 100);
+            firstShifter.Name = "first shifter";
+            firstShifter.AddSensor(new LinearSensor(new Vector3D(1, 0, 0)));
+            scene.AddBody(firstShifter);
+
+            // second shifter
+            var secondShifter = Cuboid.Create(500, 100, 100);
+            secondShifter.Name = "second shifter";
+            secondShifter.AddSensor(new LinearSensor(new Vector3D(1, 0, 0)));
+            scene.AddBody(secondShifter);
+
+            var firstAnchor = new Anchor(firstShifter, Matrix44D.CreateTranslation(new Vector3D(250, 0, 0)));
+            var secondAnchor = new Anchor(secondShifter, Matrix44D.CreateTranslation(new Vector3D(-250, 0, 0)));
+            var fixedFirstToSecondShifter = new FixedConstraint(firstAnchor, secondAnchor);
+
+            scene.InitScene();
+            return scene;
+        }
+    }
+}
---
+++ Demonstrations/LinearAxisDemo_/SeedScene.cs
@@ -0,0 +1,51 @@
+﻿using Kinematicula.Scening;
+using Kinematicula.Graphics;
+using Kinematicula.Graphics.Creators;
+using Kinematicula.Graphics.Saving;
+using Kinematicula.Kinematics.DirectForwardSolving;
+using Kinematicula.Kinematics.DirectInverseSolving;
+using Kinematicula.Mathematics;
+
+namespace CylinderDemonstration
+{
+    public static class SeedScene
+    {
+        public static Scene CreateAndPopulateScene()
+        {
+            var scene = new Scene(new DirectForwardConstraintSolver(), new DirectInverseConstraintSolver(), new SnapshotFactory(new StateFactory()));
+
+            // floor
+            var floor = Floor.Create(10, 100);
+            floor.Name = "Floor";
+            scene.AddBody(floor);
+            var fixedToWorldConstraint = new FixedConstraint(new Anchor(scene.World, Matrix44D.Identity), new Anchor(floor, Matrix44D.Identity));
+
+            // Crank socket
+            var socket = Cylinder.Create(10, 50, 200);
+            scene.AddBody(socket);
+            var floorAnchor = new Anchor(floor, Matrix44D.Identity);
+            var socketToFloorAnchor = new Anchor(socket, Matrix44D.CreateTranslation(new Vector3D(0, 0, -100)));
+            var fixedToFloorConstraint = new FixedConstraint(floorAnchor, socketToFloorAnchor);
+
+            // Crank arm
+            var arm = Oval.Create(10, 10, 50, 50, false, false, false, false, 200, 50, Matrix44D.Identity);
+            scene.AddBody(arm);
+            var socketToArmAnchor = new Anchor(socket, Matrix44D.CreateTranslation(new Vector3D(0, 0, 100)));
+            var armToSocketAnchor = new Anchor(arm, Matrix44D.CreateTranslation(new Vector3D(0, 0, -25)));
+            var rotationAxisConstraint = new RotationAxisConstraint(socketToArmAnchor, armToSocketAnchor, 45.0.ToRadiant(), -720.0.ToRadiant(), +720.0.ToRadiant());
+            arm.AddSensor(new CylinderSensor(new Vector3D(0, 0, 1)));
+
+
+            // Crank grip
+            var grip = Cylinder.Create(10, 50, 50);
+            scene.AddBody(grip);
+            var armToGrip = new Anchor(arm, Matrix44D.CreateTranslation(new Vector3D(200, 0, +25)));
+            var gripToArm = new Anchor(grip, Matrix44D.CreateTranslation(new Vector3D(0, 0, -25)));
+            var fixedToArmConstraint = new FixedConstraint(armToGrip, gripToArm);
+            grip.AddSensor(new CylinderSensor(new Vector3D(0, 0, 1), arm));
+
+            scene.InitScene();
+            return scene;
+        }
+    }
+}
