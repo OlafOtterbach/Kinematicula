401cb06cd51f01ee9ca26b5a965784785bdc8b04
Olaf Otterbach <olaf.otterbach@gmx.de>
2023-12-17 08:44:56 +0100

    Adding base functions.

--- Kinematicula/Graphics/Extensions/BodyExtensions.cs
+++ Kinematicula/Graphics/Extensions/BodyExtensions.cs
@@ -1,18 +1,51 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿namespace Kinematicula.Graphics.Extensions;
 
-namespace Kinematicula.Graphics.Extensions
+using Kinematicula.Mathematics;
+
+public static class BodyExtensions
 {
-    public static class BodyExtensions
+    public static IEnumerable<Body> GetBodyAndDescendants(this Body body)
     {
-        public static IEnumerable<Body> GetBodyAndDescendants(this Body body)
+        yield return body;
+        var children = body.Children.SelectMany(child => child.GetBodyAndDescendants());
+        foreach (var child in children)
         {
-            yield return body;
-            var children = body.Children.SelectMany(child => child.GetBodyAndDescendants());
-            foreach (var child in children)
-            {
-                yield return child;
-            }
+            yield return child;
         }
+    }
+
+    public static IEnumerable<Position3D> GetPointCloude(this IEnumerable<Body> bodies)
+    {
+        return bodies.GetPointCloude(Matrix44D.Identity);
+    }
+
+    public static IEnumerable<Position3D> GetPointCloude(this Body body)
+    {
+        return body.GetPointCloude(Matrix44D.Identity);
+    }
+
+    public static IEnumerable<Position3D> GetPointCloude(this IEnumerable<Body> bodies, Matrix44D transform)
+    {
+        var allBodies = bodies.SelectMany(body => body.GetBodyAndDescendants());
+        return GetPointCloudeFromBodies(allBodies, transform);
+    }
+
+    public static IEnumerable<Position3D> GetPointCloude(this Body body, Matrix44D transform)
+    {
+        var bodies = body.GetBodyAndDescendants();
+        return GetPointCloudeFromBodies(bodies, transform);
+    }
+
+    private static IEnumerable<Position3D> GetPointCloudeFromBodies(this IEnumerable<Body> bodies, Matrix44D transform)
+    {
+        var points = bodies.AsParallel().SelectMany(body => body.GetPointCloudeFromBody(transform));
+        return points;
+    }
+
+    private static IEnumerable<Position3D> GetPointCloudeFromBody(this Body body, Matrix44D transform)
+    {
+        var matrix = transform * body.Frame;
+        var points = body.Points.AsParallel().Select(p => matrix * p.Position);
+        return points;
     }
 }
--- Kinematicula/LogicViewing/Services/FitInView/SceneInViewFitting.cs
+++ Kinematicula/LogicViewing/Services/FitInView/SceneInViewFitting.cs
@@ -1,12 +1,74 @@
 ﻿namespace Kinematicula.LogicViewing.Services.FitInView;
 
 using Kinematicula.Graphics;
+using Kinematicula.Graphics.Extensions;
+using Kinematicula.Mathematics;
 using Kinematicula.Scening;
 
 public static class SceneInViewFitting
 {
+    private struct BoundedBox
+    {
+        public double MinX { get; set; }
+        public double MinY { get; set; }
+        public double MinZ { get; set; }
+        public double MaxX { get; set; }
+        public double MaxY { get; set; }
+        public double MaxZ { get; set; }
+    }
+
+    private struct CardinalDirections
+    {
+        public Position3D North { get; set; }
+        public Position3D West { get; set; }
+        public Position3D East { get; set; }
+        public Position3D South { get; set; }
+    }
+
     public static void FitInView(this Scene scene, Camera camera)
     {
+        var cameraSystem = camera.Frame.Inverse();
+        var pointCloude = scene.Bodies.GetPointCloude(cameraSystem).ToList();
 
+
+
+    }
+
+    private static BoundedBox MergeToBoundedBox(BoundedBox first, Position3D pos)
+    {
+        var boundedBox = new BoundedBox()
+        {
+            MinX = Math.Min(first.MinX, pos.X),
+            MinY = Math.Min(first.MinY, pos.Y),
+            MinZ = Math.Min(first.MinY, pos.Z),
+            MaxX = Math.Max(first.MaxX, pos.X),
+            MaxY = Math.Max(first.MaxY, pos.Y),
+            MaxZ = Math.Max(first.MaxZ, pos.Z),
+        };
+
+        return boundedBox;
+    }
+
+    /// <summary>
+    /// Calculates distance from position to face.
+    /// </summary>
+    /// <param name="position">Position</param>
+    /// <param name="offset">Offset of face</param>
+    /// <param name="normal">Normal of face</param>
+    /// <returns>Distance</returns>
+    public static double DistancePositionToPlane(this Position3D position, Position3D offset, Vector3D normal)
+    {
+        var nx = normal.X;
+        var ny = normal.Y;
+        var nz = normal.Z;
+        var x = offset.X;
+        var y = offset.Y;
+        var z = offset.Z;
+        var px = position.X;
+        var py = position.Y;
+        var pz = position.Z;
+        var direction = (px - x) * nx + (py - y) * ny + (pz - z) * nz;
+        var dist = Math.Abs(direction);
+        return dist;
     }
 }
